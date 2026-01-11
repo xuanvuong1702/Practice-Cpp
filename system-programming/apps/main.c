ca4a3ee59f6e669fc54d672d25dc83416b53a781
phung xuan vuong <phungxuanvuong97>
2026-01-11 11:15:13 +0700

    update git ignore

--- system-programming/apps/main.c
+++ system-programming/apps/main.c
@@ -1,13 +1,34 @@
-#include "string_struct.h"
-#include "forking.h"
 #include <stdio.h>
+#include <stdarg.h>
 
+#ifdef DEBUG
+#define log(...) __log(__LINE__, __FILE__, __VA_ARGS__)
+#else
+#define log(...)  // Không in gì trong production
+#endif
 
-int main()
-{
-    //chapter 1 part 4: string and struct
-    //RunMemoryManagement();
-    SimpleFork();
-
+int __log(int line, const char *file, const char *fmt, ...) {
+    fprintf(stderr, "|%s:%d| ", file, line);
+    
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+    
+    fprintf(stderr, "\n");
     return 0;
 }
+
+int main() {
+    int x = 42;
+    log("Value of x: %d", x);  // Chỉ in khi compile với -DDEBUG
+    
+    x = x * 2;
+    log("After multiply: %d", x);
+    
+    return 0;
+}
+
+// Compile: gcc -DDEBUG -o program program.c
+// Output: |program.c:15| Value of x: 42
+//         |program.c:18| After multiply: 84
